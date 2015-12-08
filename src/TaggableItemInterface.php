f3044da85832066155e8d6365062d63883ce674b
Tobias Nyholm <tobias.nyholm@gmail.com>
2015-12-08 18:16:41 +0100

    Remove setTags

--- src/TaggableItemInterface.php
+++ src/TaggableItemInterface.php
@@ -5,15 +5,6 @@ namespace Cache\Taggable;
 interface TaggableItemInterface
 {
     /**
-     * An array with strings. If you attempt to change tags once already set an exception must be thrown.
-     *
-     * @param array $tags
-     *
-     * @return $this
-     */
-    public function setTags(array $tags);
-
-    /**
      * Return an array of the tags.
      *
      * @return array
